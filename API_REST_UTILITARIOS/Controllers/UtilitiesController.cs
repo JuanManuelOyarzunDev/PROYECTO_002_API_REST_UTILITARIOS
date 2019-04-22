01770a72ab6736bb0b52afccff67df14cf801a5b
JuanManuelOyarzunDev <juan.oyarzun.gajardo@hotmail.com>
2019-04-22 15:42:25 -0400

    prueba de actualizacion

--- API_REST_UTILITARIOS/Controllers/UtilitiesController.cs
+++ API_REST_UTILITARIOS/Controllers/UtilitiesController.cs
@@ -28,7 +28,7 @@ namespace API_REST_UTILITARIOS.Controllers
             EmailModel resultado_operacion = new EmailModel(id,message);
 
             resultado_operacion.Id = "1";
-            resultado_operacion.Message = "PRUEBA DE CAMBIOS 11.00000001";
+            resultado_operacion.Message = "PRUEBA DE CAMBIOS 11.0000000001";
 
 
             return resultado_operacion;

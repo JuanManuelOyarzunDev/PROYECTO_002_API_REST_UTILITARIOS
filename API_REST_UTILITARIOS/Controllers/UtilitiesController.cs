b0af7e1979319823b8c796400e567c1d778c748d
JuanManuelOyarzunDev <juan.oyarzun.gajardo@hotmail.com>
2019-04-22 12:27:14 -0400

    sdfsdfsd

--- API_REST_UTILITARIOS/Controllers/UtilitiesController.cs
+++ API_REST_UTILITARIOS/Controllers/UtilitiesController.cs
@@ -28,7 +28,7 @@ namespace API_REST_UTILITARIOS.Controllers
             EmailModel resultado_operacion = new EmailModel(id,message);
 
             resultado_operacion.Id = "1";
-            resultado_operacion.Message = "PRUEBA DE CAMBIOS 10.01";
+            resultado_operacion.Message = "PRUEBA DE CAMBIOS 10.00001";
 
             return resultado_operacion;
         }
